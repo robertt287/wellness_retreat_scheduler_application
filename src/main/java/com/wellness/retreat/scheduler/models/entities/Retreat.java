53901b0c212bc6e7b0a3e74947e2b39ba58457fd
robertt287 <dragomirr94@gmail.com>
2024-01-11 22:20:09 +0200

    Added Entities :  Booking , Retreat and User classes

---
+++ src/main/java/com/wellness/retreat/scheduler/models/entities/Retreat.java
@@ -0,0 +1,29 @@
+package com.wellness.retreat.scheduler.models.entities;
+
+import java.time.LocalDate;
+
+public class Retreat {
+
+    private Long id;
+
+
+    private String name;
+
+
+    private String description;
+
+
+    private LocalDate startDate;
+
+
+    private LocalDate endDate;
+
+
+    private String location;
+
+
+    private double price;
+
+
+    private int capacity;
+}
---
+++ src/main/java/com/wellness/retreat/scheduler/models/entities/User.java
@@ -0,0 +1,19 @@
+package com.wellness.retreat.scheduler.models.entities;
+
+public class User {
+
+
+    private Long id;
+
+
+    private String fullName;
+
+
+    private String email;
+
+
+    private String phone;
+
+
+    private String address;
+}
